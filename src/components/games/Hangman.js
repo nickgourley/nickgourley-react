ab15087871578da906a4ca45a000507bcb81940f
Nick Gourley <nick.gourley@gmail.com>
2020-11-07 14:34:47 -0500

    fix script

--- src/components/games/Hangman.js
+++ src/components/games/Hangman.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 class Hangman extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            game: true
+        }
+    }
     componentDidMount() {
         let canvas = document.getElementById('canvas');
         let ctx = canvas.getContext('2d');
@@ -12,6 +18,7 @@ class Hangman extends React.Component {
     return (
         <div>
             <h1>Hangman</h1>
+            { this.state.game && <h1>hello from state</h1> }
             <canvas id="canvas" height="250" width="250" style={{border: "solid black 1px"}}></canvas>
         </div>
     );
